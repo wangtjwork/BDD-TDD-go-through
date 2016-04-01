6f575c2067c7a50034aeb4b2954c22fe29683368
wangtjwork <wangpanda1993@tamu.edu>
2016-04-01 05:24:41 +0000

    does it really have to be so hard

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,43 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /^the following movies exist:$/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create!(movie)
+    
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+  end
+end
+
+
+Then /^the director of "([^"]*)" should be "([^"]*)"$/ do |arg1, arg2|
+   assert page.body =~ /#{arg1}.+Director.+#{arg2}/m
+   # express the regexp above with the code you wish you had
+end
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  assert page.body =~ /#{e1}.+#{e2}/m , "Wrong order"
+  #  ensure that that e1 occurs before e2.
+  #  page.content  is the entire content of the page as a string.mo
+end
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  if uncheck == "un"
+    rating_list.split(', ').each {|x| step %{I uncheck "ratings_#{x}"}}
+  else
+    rating_list.split(', ').each {|x| step %{I check "ratings_#{x}"}}
+  end
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+end
+
+Then /I should not see any of the movies/ do
+  rows = page.all('#movies tr').size - 1
+  assert rows == 0
+end
+
+Then /I should see all of the movies/ do
+  rows = page.all('#movies tr').size - 1
+  assert rows == Movie.count()
+end
