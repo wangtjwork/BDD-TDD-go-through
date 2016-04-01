e557031dba4e4dc7be0d461185a3261332fc1347
wangtjwork <wangpanda1993@tamu.edu>
2016-04-01 05:36:29 +0000

    yes it works

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992', :director => 'George Lucas'},
+movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
     	  {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
     	  {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
       	  {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
@@ -16,10 +16,6 @@ movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992', 
       	  {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'},
       	  {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
       	  {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000'},
-      	  {:title => 'Star Wars', :rating => 'PG', :release_date => '25-May-1977', :director => 'George Lucas'},
-      	  {:title => 'Blade Runner', :rating => 'PG', :release_date => '25-Jun-1982', :director => 'Ridley Scott' },
-      	  {:title => 'Alien', :rating => 'R', :release_date => '25-May-1979'},
-      	  {:title => 'THX-1138', :rating => 'r', :release_date => '11-Mar-1971'},
   	 ]
 
 movies.each do |movie|
